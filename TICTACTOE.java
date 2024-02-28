638b4fe3a1b9d7b5235afaf7f1256b366d9c9542
RSimmondsIV <RSimmondsIV2006@gmail.com>
2024-02-28 09:54:56 -0500

    Co-authored-by: alian912 <alian912@users.noreply.github.com> Co-authored-by: Sofiane-Lachab <Sofiane-Lachab@users.noreply.github.com>

---
+++ TICTACTOE.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+import javax.swing.text.Position;
+public class TICTACTOE 
+{
+    private static int[][] board = new int[3][3]; // 1: X, -1: O, 0: blank
+    private int cWins = 0; //computer wins
+    private int hWins = 0; //human wins
+    private int position;
+    public static void main(String[] args) 
+    {
+        for(int i = 0; i < board.length; i++)
+        {
+            for(int j = 0; j < board[i].length; j++)
+            {
+                board[i][j] = 0;
+            }
+        }
+
+        Scanner input = new Scanner(System.in);
+    
+    }
+    
+    public static boolean hasWon(int[][] board)
+    {
+        boolean result = false;
+        
+        for (int i = 0; i < 3; i++) { // 3 possible configurations for a win
+            if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+                result = true;
+            }
+        }
+
+        if ((board [0][0] == board[1][1] && board[0][0] == board[2][2]) || (board[0][2] == board[1][1] && board[0][2] == board[0][2]))
+            result = true;
+
+
+        return result;
+    }
+
+    public static boolean freeSpace(int position){
+        boolean result = false;
+        int x = (position%3 ), y = t;
+        return result;
+    }
+}
